22d74578315e857c3a89934ad610733b996fd7dd
umairkhalid07 <umairk911.uk@gmail.com>
2022-09-03 17:40:08 +0500

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,35 @@
+const chatList = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
+const newNameForm = document.querySelector('.new-name');
+const updateMssg = document.querySelector('.update-mssg');
+const rooms = document.querySelector('.chat-rooms');
+
+newChatForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const message = newChatForm.message.value.trim();
+    chatroom.addChat(message)
+        .then(() => newChatForm.reset())
+        .catch(err => console.log(err));
+});
+
+newNameForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const newName = newNameForm.name.value.trim();
+    chatroom.updateName(newName);
+    newNameForm.reset();
+    updateMssg.innerText = `Your name was updated to ${newName}`;
+    setTimeout(() => updateMssg.innerText = '', 3000);
+});
+
+rooms.addEventListener('click', e => {
+    if(e.target.tagName === 'BUTTON'){
+        chatUI.clear();
+        chatroom.updateRoom(e.target.getAttribute('id'));
+        chatroom.getChats(chat => chatUI.render(chat));
+    }
+});
+
+const username = localStorage.username ? localStorage.username : 'New User';
+const chatUI = new ChatUI(chatList);
+const chatroom = new Chatroom('general', username);
+chatroom.getChats(data => chatUI.render(data));
---
+++ scripts/chat.js
@@ -0,0 +1,43 @@
+class Chatroom {
+    constructor(room, username){
+        this.room = room;
+        this.username = username;
+        this.chats = db.collection('chats');
+        this.unsub;
+    }
+    async addChat(message){
+        const now = new Date();
+        const chat = {
+            message: message,
+            username: this.username,
+            room: this.room,
+            created_at: firebase.firestore.Timestamp.fromDate(now)
+        };
+      
+        const response = await this.chats.add(chat);
+        return response;
+    }
+    getChats(callback){
+        this.unsub = this.chats
+            .where('room', '==', this.room)
+            .orderBy('created_at')
+            .onSnapshot(snapshot => {
+                snapshot.docChanges().forEach(change => {
+                    if(change.type === 'added'){
+                        callback(change.doc.data());
+                     }
+                });
+            });
+    }
+    updateName(username){
+      this.username = username;
+      localStorage.username = username;
+    }
+    updateRoom(room){
+        this.room = room;
+        console.log('room updated');
+        if(this.unsub){
+            this.unsub();
+        }
+    }
+}
